c273a1c2b97c1ab0c74f470d8e7e40d9209c2908
Clemens Rudert <crud3_rt@posteo.ch>
2025-10-19 10:14:27 +0200

    Implement a class to be used for passing strings around and to avoid that they may be printed or logged somewhere accidently.
    Implement serializable and typed variant of the pg_service.conf for solid handling of those information more easy.

--- src/qgis_server_light/interface/common.py
+++ src/qgis_server_light/interface/common.py
@@ -2,7 +2,9 @@
 
 """
 from dataclasses import dataclass
+from dataclasses import field
 from dataclasses import fields
+from enum import Enum
 
 
 @dataclass
@@ -75,7 +77,6 @@ class BaseInterface:
         cls = self.__class__.__name__
         for field in fields(self):
             value = getattr(self, field.name)
-            print(field.name)
             if field.name in self.redacted_fields:
                 members.append(f"{field.name}=**REDACTED**")
             elif (
@@ -88,3 +89,63 @@ class BaseInterface:
             else:
                 members.append(f"{field.name}={value!r}")
         return f"{cls}({', '.join(members)})"
+
+
+class RedactedString(str):
+    """
+    This special string class can be used to handle secret strings in the application. It works like a normal
+    string but in case its used to print or log its value is not revieled to the output.
+    """
+
+    def __new__(cls, value, redacted_text="**REDACTED**"):
+        obj = super().__new__(cls, value)
+        obj._redacted_text = redacted_text
+        return obj
+
+    def __str__(self):
+        return self._redacted_text
+
+    def __repr__(self):
+        return f"<RedactedString {self._redacted_text}>"
+
+    def reveal(self):
+        """
+        Allows access to the original value when neccesary.
+
+        Returns:
+            The secret string.
+        """
+
+        return super().__str__()
+
+
+class SslMode(str, Enum):
+    DISABLE = "disable"
+    ALLOW = "allow"
+    PREFER = "prefer"
+    REQUIRE = "require"
+    VERIFY_CA = "verify-ca"
+    VERIFY_FULL = "verify-full"
+
+
+@dataclass(repr=False)
+class PgServiceConf(BaseInterface):
+    """
+    A typed definition of the pg_service.conf definition which might be used.
+    """
+
+    name: str = field(metadata={"type": "Element"})
+    host: str = field(metadata={"type": "Element"}, default=None)
+    port: int = field(metadata={"type": "Element"}, default=None)
+    user: str = field(metadata={"type": "Element"}, default=None)
+    dbname: str = field(metadata={"type": "Element"}, default=None)
+    password: str = field(metadata={"type": "Element"}, default=None)
+    sslmode: SslMode = field(metadata={"type": "Element"}, default=SslMode.PREFER.value)
+    application_name: str = field(metadata={"type": "Element"}, default=None)
+    client_encoding: str = field(metadata={"type": "Element"}, default="UTF8")
+    # possibilitiy to link to another service (nested definitions!)
+    service: str = field(metadata={"type": "Element"}, default=None)
+
+    @property
+    def redacted_fields(self) -> set:
+        return {"password"}
